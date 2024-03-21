f709f9ca310b76302e1eb852355e14ca3e7a5a5c
Francisco Cantos <FranciscoCantosGutierrez@gmail.com>
2024-03-21 18:40:57 +0100

    Adds extension in utils
    Signed-off-by: Francisco Cantos <FranciscoCantosGutierrez@gmail.com>

---
+++ CryptoCoins/Utils/Extensions/Result.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+extension Result {
+    var failureValue: Error? {
+        switch self {
+        case .failure(let error):
+            return error
+        case .success:
+            return nil
+        }
+    }
+}
