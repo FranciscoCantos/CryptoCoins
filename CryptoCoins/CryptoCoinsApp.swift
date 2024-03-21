b17ee1e50263134e46ca3f1b365aad77e9fbb460
Francisco Cantos <FranciscoCantosGutierrez@gmail.com>
2024-03-21 18:39:43 +0100

    First commit
    Signed-off-by: Francisco Cantos <FranciscoCantosGutierrez@gmail.com>

---
+++ CryptoCoins/CryptoCoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinsApp.swift
+//  CryptoCoins
+//
+//  Created by Kurro on 21/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
